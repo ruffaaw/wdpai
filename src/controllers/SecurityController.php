c46c370781d8161173ec85a894519dec09634fbf
ruffaaw <rafalciupek07@gmail.com>
2023-02-13 00:16:56 +0100

    Connect to database, install pdo drivers, create user repository

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,12 +2,14 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
+
 
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new User('jsnow@pk.edu.pl', 'admin', 'John', 'Snow');
+        $userRepository = new UserRepository();
 
         if(!$this->isPost()) {
             return $this->login('login');
@@ -16,6 +18,11 @@ class SecurityController extends AppController
         $email = $_POST["email"];
         $password = $_POST["password"];
 
+        $user = $userRepository->getUser();
+
+        if(!$user) {
+            $this->render('login', ['messages' => ['User not exist!']]);
+        }
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
         }
