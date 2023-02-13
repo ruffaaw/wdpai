9d5087b2abbc1904c594cf7f5d50771aa291cd7e
ruffaaw <rafalciupek07@gmail.com>
2023-02-13 20:28:58 +0100

    Added password encryption

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -8,14 +8,16 @@ require_once __DIR__.'/../repository/UserRepository.php';
 class SecurityController extends AppController
 {
 
-    function hash_password($password) {
+    function hash_password($password): string
+    {
         $options = [
             'cost' => 12,
         ];
         return password_hash($password, PASSWORD_BCRYPT, $options);
     }
 
-    function verify_password($password, $hashed_password) {
+    function verify_password($password, $hashed_password): bool
+    {
         if (password_verify($password, $hashed_password)) {
             return true;
         } else {
@@ -47,8 +49,6 @@ class SecurityController extends AppController
         if (!$this->verify_password($password, $user->getPassword())) {
             return $this->render('login', ['messages' => ["Wrong password"]]);
         }
-
-/*        return $this->render('projects');*/
 
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/homepage");
